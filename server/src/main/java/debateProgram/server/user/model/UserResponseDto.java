a883ddc4d95b2eeffd53358c32bc5ffe13269b26
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2022-09-19 17:23:43 +0900

    feat: updateInfo, delete user

--- server/src/main/java/debateProgram/server/user/model/UserResponseDto.java
+++ server/src/main/java/debateProgram/server/user/model/UserResponseDto.java
@@ -5,12 +5,15 @@ import lombok.Getter;
 
 @Getter
 @Builder
-public class UserRequestDto {
+public class UserResponseDto {
 
     private int userCode;
+    private String discussionState;
+    private int kakaoId;
     private String nickname;
     private String profileImg;
     private String kakaoEmail;
     private String userLikes;
+    private String userRole;
 
 }
